5774c8f86ed81eaf811ddcdf3d4115344a39d7ea
Daniel master lazar <daniel.lazar@htdm.cz>
2023-09-11 11:23:20 +0200

    improvement for size and added script which builds everything at once

---
+++ scripts/build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT_PATH=$(pwd | grep -o ".*/inlaserworld_engine")
+
+echo "$ROOT_PATH"
+
+cd "$ROOT_PATH" || exit 1
+
+for path in $(find "$ROOT_PATH/plugins" -name BUILD.mk); do
+        for buildPath in $(find $(dirname $path) -name Makefile); do
+              if [[ "$(dirname $buildPath)" == *-o ]]; then
+                  make -C $(dirname $buildPath) || exit 1
+              fi
+        done
+done
